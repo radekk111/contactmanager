cb30e8de7571b6ab44c3055b8f774deb180c04ea
Radovan Tkac <tkac.radovan@gmail.com>
2019-05-08 20:26:12 +0200

    Updates

---
+++ precache-manifest.f01b497a938241bd9b63a07f15c35148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1899d5db0a173981192b9408e833f8df",
+    "url": "/radekk111/contactmanager/index.html"
+  },
+  {
+    "revision": "92c067c679ef105ddc7e",
+    "url": "/radekk111/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "426278f7a8d591944430",
+    "url": "/radekk111/contactmanager/static/css/main.bbadad04.chunk.css"
+  },
+  {
+    "revision": "92c067c679ef105ddc7e",
+    "url": "/radekk111/contactmanager/static/js/2.e165e39e.chunk.js"
+  },
+  {
+    "revision": "426278f7a8d591944430",
+    "url": "/radekk111/contactmanager/static/js/main.674bb77c.chunk.js"
+  },
+  {
+    "revision": "34b5c76a456f629588a9",
+    "url": "/radekk111/contactmanager/static/js/runtime~main.62b69b3e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+
+importScripts(
+  "/radekk111/contactmanager/precache-manifest.f01b497a938241bd9b63a07f15c35148.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/radekk111/contactmanager/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
